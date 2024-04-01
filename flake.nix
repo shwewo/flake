c2cb43d52c61be4707322ee1d68e15932f14d187
shwewo <shwewo@gmail.com>
2024-04-01 11:02:16 +0300

    removed unfree license, google chrome is already unfree...

--- flake.nix
+++ flake.nix
@@ -10,14 +10,16 @@
 
   outputs = inputs @ { self, nixpkgs, flake-utils, stable, tdesktop }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      {
+      let 
+        pkgs = import nixpkgs.legacyPackages.${system} { system = system; config.allowUnfree = 1; };
+        stable = import nixpkgs.legacyPackages.${system} { system = system; config.allowUnfree = 1; };
+      in {
         packages = {
           audiorelay = pkgs.callPackage ./derivations/audiorelay.nix {};
           namespaced = pkgs.callPackage ./derivations/namespaced.nix {};
           ephemeralbrowser = pkgs.callPackage ./derivations/ephemeralbrowser.nix {};
           ruchrome = pkgs.callPackage ./derivations/ruchrome.nix {};
-          spotify = pkgs.callPackage ./derivations/spotify.nix { spotify = inputs.stable.legacyPackages.${system}.spotify; };
+          spotify = pkgs.callPackage ./derivations/spotify.nix { spotify = stable.spotify; };
           microsocks = pkgs.callPackage ./derivations/microsocks.nix {};
           playit = pkgs.callPackage ./derivations/playit.nix {};
           tdesktop = inputs.tdesktop.packages.${system}.default;
