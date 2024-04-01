8541c64fdfdd2ee4f9096723dfe8aa7a5e79c4a4
shwewo <shwewo@gmail.com>
2024-04-01 09:14:47 +0300

    first commit

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
-  description = "Shwewo flake with custom programs";
+  description = "Shwewo's flake with custom programs";
   
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     stable.url = "github:nixos/nixpkgs/nixos-23.11";
     tdesktop.url = "github:nixos/shwewo/telegram-desktop-patched";
   };
@@ -16,7 +16,7 @@
           audiorelay = pkgs.callPackage ./derivations/audiorelay.nix {};
           namespaced = pkgs.callPackage ./derivations/namespaced.nix {};
           ephemeralbrowser = pkgs.callPackage ./derivations/ephemeralbrowser {};
-          spotify = pkgs.callPackage ./derivations/spotify.nix {};
+          spotify = pkgs.callPackage ./derivations/spotify.nix { spotify = inputs.stable.legacyPackages.${system}.spotify; };
           microsocks = pkgs.callPackage ./derivations/microsocks.nix {};
           playit = pkgs.callPackage ./derivations/playit.nix {};
           tdesktop = inputs.tdesktop.packages.${pkgs.system}.default;
