fa1311e31e45577af3116e98dcb3a748e900c912
shwewo <shwewo@gmail.com>
2024-08-13 23:47:55 +0300

    spoofdpi

--- derivations/spoofdpi.nix
+++ derivations/spoofdpi.nix
@@ -1,18 +1,19 @@
 { pkgs, stdenv }:
 
 stdenv.mkDerivation {
-  pname = "linux-router";
+  pname = "spoof-dpi";
   version = "0.7.3";
 
   dontBuild = true;
+  sourceRoot = ".";
 
   src = pkgs.fetchurl {
     url = "https://github.com/xvzc/SpoofDPI/releases/download/v0.10.6/spoof-dpi-linux-amd64.tar.gz";
-    hash = "";
+    hash = "sha256-5I0no/w90d56DXgKbakWdNymmkpBYUy5SZnakKgFWSo=";
   };
 
   installPhase = ''
     mkdir -p $out/bin/
-    cp $src/spoof-dpi $out/bin/
+    cp ./spoof-dpi $out/bin/
   '';
 }
