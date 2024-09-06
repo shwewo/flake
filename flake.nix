23faf804456cb59191f3331a56f833fa06c68bb2
shwewo <shwewo@gmail.com>
2024-09-06 04:02:01 +0300

    ...

--- flake.nix
+++ flake.nix
@@ -3,23 +3,25 @@
   
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-spotify.url = "github:nixos/nixpkgs?rev=651b4702e27a388f0f18e1b970534162dec09aff";
     flake-utils.url = "github:numtide/flake-utils";
     tdesktop.url = "github:shwewo/telegram-desktop-patched";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, flake-utils, tdesktop }:
+  outputs = { self, nixpkgs, nixpkgs-stable, nixpkgs-spotify, flake-utils, tdesktop }:
     flake-utils.lib.eachDefaultSystem (system:
       let 
         pkgs = import nixpkgs { system = system; config.allowUnfree = true; };
         stable = import nixpkgs-stable { system = system; config.allowUnfree = true; };
+        spotify = import nixpkgs-spotify { system = system; config.allowUnfree = true; };
       in {
         packages = with pkgs; {
           audiorelay = callPackage ./derivations/audiorelay.nix {};
           namespaced = callPackage ./derivations/namespaced.nix {};
           ephemeralbrowser = callPackage ./derivations/ephemeralbrowser.nix {};
           ruchrome = callPackage ./derivations/ruchrome.nix {};
-          spotify = callPackage ./derivations/spotify.nix { spotify = stable.spotify; };
+          spotify = callPackage ./derivations/spotify.nix { spotify = spotify.spotify; };
           microsocks = callPackage ./derivations/microsocks.nix {};
           playit = callPackage ./derivations/playit.nix {};
           lnxrouter = callPackage ./derivations/lnxrouter.nix { useHaveged = true; };
